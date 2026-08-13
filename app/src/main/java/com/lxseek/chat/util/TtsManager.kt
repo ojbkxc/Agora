cbf3d58035f94212bc17586a90f53d9677d5940d
ojbkxc <1657642978@qq.com>
2026-08-14 00:01:40 +0800

    fix: add missing kotlinx.coroutines.launch import

--- app/src/main/java/com/lxseek/chat/util/TtsManager.kt
+++ app/src/main/java/com/lxseek/chat/util/TtsManager.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
