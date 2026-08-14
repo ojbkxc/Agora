5e554ff5f37c75a4a789199cca4c05ccf2831086
ojbkxc <1657642978@qq.com>
2026-08-14 15:33:00 +0800

    v1.0.36: remove dead AsrModelManager + 11 orphaned ASR strings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.lxseek.chat"
         minSdk = 24
         targetSdk = 36
-        versionCode = 36
-        versionName = "1.0.35"
+        versionCode = 37
+        versionName = "1.0.36"
 
 
         ndk {
